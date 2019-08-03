f73ed4c8868eb27bcd7225381d4dfaabbb10f5c1
Vincent Philaeux Lamotte <philaeux@the-cluster.org>
2019-08-03 03:45:09 +0200

    Initial kotlin repository

---
+++ dota2stats_backend/test/ApplicationTest.kt
@@ -0,0 +1,30 @@
+package org.the_cluster.dota2stats
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.locations.*
+import io.ktor.features.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("HELLO WORLD!", response.content)
+            }
+        }
+    }
+}
